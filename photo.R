bc2bacd0afe116d1036d45c72fa642bb6742752a
Benno <bp2014@online.de>
2019-06-17 13:24:24 +0200

    refactored and improved

--- photo.R
+++ photo.R
@@ -1,52 +1,102 @@
-dof <- function(d,
-                f   = 85,
+# f: focal length of lens        [mm]
+# F: F-stop
+# d: distance (set on lens)      [m]
+# COC: circle of confusion       [mm]
+
+# Depth of Field (DOF)
+dof <- function(d,                   # [m]
+                f   = 85,            # [mm]
                 F   = 1.2,
-                COC = 0.03){
+                COC = 0.03,
+                verbose = getOption('verbose')){         # [mm]
     H <- f^2/(F*COC)
     dmm <- d*1000
     n <- H*dmm
     dd <- dmm - f
     near <- n/(H+dd)
     far <- n/(H-dd)
-    if(far<0) far <- 1e8
-    sprintf("%f - %f - %f\n", round(near/1000,4), round((far-near)/1000,4), round(far/1000,4))
+    if(far<0) far <- Inf   # beyond hyperfocal distance, -> Inf
+    if(verbose){
+        cat(sprintf("%f - %f - %f\n",
+                    round(near/1000,4),
+                    round((far-near)/1000,4),
+                    round(far/1000,4)))
+    }
     return(c(near,far)/1000)
 }
 
+# near/far focus
 get.nf <- function(f, F, min=1, max=100, COC = 0.03, ...){
-    H <- f^2/(F*COC)
-    x <- as.vector(outer(c(1,1.2,1.5,1.7,2,2.5,3,4,5,6,7,8,9), 10^seq(0, ceiling(log10(max)))))
-    nf <- rbind(x,sapply(x, FUN=dof, f=f, F=F, COC=COC))
-    return(nf[,nf[1,]<=max])
-}
-
-plot.nf <- function(nf, add = FALSE, ...){
-    if (!add){
-        plot(c(0,max(nf[1,])), c(0, max(nf[3,])),
-             type = 'n',
-             xlab='Distance', ylab='Focus',
-             xlim = c(0, max(nf[1,])))
-        lines(nf[1,], nf[1,], lty = 2)
+    H <- HFD(f, F, COC)
+    #x <- as.vector(outer(c(1,1.2,1.5,1.7,2,2.5,3,4,5,6,7,8,9), 10^seq(0, ceiling(log10(max)))))
+    x <- min:max
+    nf <- data.frame(x, t(sapply(x, FUN=dof, f=f, F=F, COC=COC)))
+    if(max>H/1000){
+        cat(sprintf("HFD(%dmm,%2g): %.2fm\n",f, F, H/1000))
     }
-    lines(nf[1,], nf[2,], ...)
-    lines(nf[1,], nf[3,], ...)
+    dimnames(nf) <- list(x,
+                         c('dist', 'near', 'far'))
+    nf <- within(nf,
+                 range <-  far-near)
+    return(nf[nf[,1]<=max,])
 }
 
-full.nf.plot <- function(f, F0, Fn, ...){
+get.all.nf <- function(f, F0, Fn=22, ...){
     #Fseq <- c(1.0, 1.2, 1.4, 1.6, 1.8, 2, 2.5, 2.8, 3.5, 4, 4.5, 5, 5.6, 6.3, 7.1, 8,
     #)
-    Fseq <- c(1.0, 1.4,  2,  2.8, 4, 5.6, 8, 11, 16, 22, 32)
-    Fs <- sort(c(F0, Fn, Fseq))
+    Fseq <- c(1.0, 1.4,  2,  2.8, 4, 5.6, 8, 11, 16, 22, 32, 44, 64)
+    Fs <- unique(sort(c(F0, Fn, Fseq)))
     Fs <- Fs[Fs >= F0 & Fs <= Fn]
     sq <- seq_along(Fs)
     cols <- rainbow(length(sq), end = 0.6)
-    for (i in sq){
-        cat(Fs[i],'\n')
-        nf <- get.nf(f, Fs[i], ...)
-        plot.nf(nf, add = i>1, ..., col = cols[i])
-    }
-
+    all.nf <- sapply(Fs,
+                     function(F) as.matrix(get.nf(f, F, ...)),
+                     simplify = 'array')
+    dimnames(all.nf) <- list(distance = all.nf[,1,1],
+                             focus    = c('dist', 'near', 'far','range'),
+                             aperture = Fs)
+    return(all.nf)
 }
+
+plot.nf <- function(nf, f=NULL, add = FALSE, ...){
+    if (!add){
+        plot(c(0, max(nf[,1])),
+             c(0, min(max(nf[,3]), 4*max(nf[,1]) )),
+             type = 'n',
+             xlab = 'Focus Distance',
+             ylab = 'Focus Range',
+             main = sprintf('Near and far focusing limits [f=%dmm]', f))
+        lines(nf[,1], nf[,1], lty = 2, col  = 'lightgray')
+    }
+    lines(nf[,1], nf[,2], ...)
+    lines(nf[,1], nf[,3], ...)
+}
+
+full.nf.plot <- function(f, F0, Fn=22, maxfr=NULL, ...){
+    #Fseq <- c(1.0, 1.2, 1.4, 1.6, 1.8, 2, 2.5, 2.8, 3.5, 4, 4.5, 5, 5.6, 6.3, 7.1, 8,
+    #)
+    Fseq <- c(1.0, 1.4,  2,  2.8, 4, 5.6, 8, 11, 16, 22, 32)
+    Fs <- unique(sort(c(F0, Fn, Fseq)))
+    Fs <- Fs[Fs >= F0 & Fs <= Fn]
+    sq <- seq_along(Fs)
+    cols <- rainbow(length(sq), end = 0.6)
+    all.nf <- get.all.nf(f, F0, Fn=22, ...)
+
+    new <- TRUE
+    for (i in c(max(sq),sq)){
+        plot.nf(all.nf[,,i], f=f, add = !new, ..., col = cols[i])
+        if(new) grid()
+        new <- FALSE
+    }
+    legend('topleft',
+           legend = dimnames(all.nf)[[3]],
+           title = "F",
+           lwd = 1,
+           col = cols,
+           bg="#FFFFFFa0")
+    return(invisible(all.nf))
+}
+
 f.values <- function(by=1){
     Fseq <- c(1.0, 1.1,1.2, 1.4, 1.6, 1.8,
               2, 2.2,2.5, 2.8, 3.2,3.5,
@@ -56,6 +106,12 @@ f.values <- function(by=1){
               32,36,40,44,50,56,
               64)
     return(Fseq[seq(1,length(Fseq), by=by)])
+}
+
+# Hyperfocal distance
+#
+HFD <- function(f, F, COC = 0.03){
+    return(f^2/(F*COC))  # [mm]!
 }
 
 HFD.plot <- function(f,
@@ -69,7 +125,7 @@ HFD.plot <- function(f,
     pc <- rep(c('+','.','.'), length = length(Fseq))
     use <- Fseq>=F0
     useF <- Fseq[use]
-    H <- f^2/(useF*COC)
+    H <- HFD(f, useF, COC)
 
     if(add){
         points(useF, H/1000,
