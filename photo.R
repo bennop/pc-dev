35d06e5239409db5154564d0994aef09e996c770
Benno <bp2014@online.de>
2019-06-21 20:17:15 +0200

    started roxygen comments

--- photo.R
+++ photo.R
@@ -1,21 +1,47 @@
-# f: focal length of lens        [mm]
-# F: F-stop
-# d: distance (set on lens)      [m]
-# COC: circle of confusion       [mm]
+## f: focal length of lens        [mm]
+## F: F-stop
+## d: distance (set on lens)      [m]
+## COC: circle of confusion       [mm]
 
-go <- function(){
+
+##' Simple knit and typeset
+##'
+##' simplify typesetting process outside of RStudiox
+##' @title go
+##' @return none
+##' @author Benno Pütz \email{puetz@@psych/mpg.de}
+##' @param n number of LaTeX runs
+##' @param bib run BibTeX? (not yet implemented)
+##' @param index create index? (not yet implemented)
+go <- function(n=1,
+               bib = FALSE,
+               index = FALSE
+               target = 'photo'){
     require (knitr)
     od <- setwd('~/Work/git/Photo')
     on.exit(setwd(od))
-    knit('photo.Rnw')
-    system('pdflatex photo')
+    knit(paste0(target,'.Rnw'))
+    for (i in 1:n){
+        system(paste('pdflatex', target))
+    }
 }
 
-# Standard F stops
 #
+#
+##' Standard F stops
+##'
+##' F-stops in steps of 1/3, setting \code{by} to 3 or 6 gives steps of 1 (i.e., half the light) or 2 (one quarter per step).
+##'
+##' Starting and final aperture are inserted if they are not part of the requested series.
+##' ##' @title f.values
+##' @param from maximum (starting) aperture
+##' @param to minimum aperture
+##' @param by stride  in sequence to return, see details
+##' @return vector of F-stops
+##' @author Benno Pütz \email{puetz@@psych/mpg.de}
 f.values <- function(from = 1.0,
                      to   = 22,
-                     by   = 1){
+                     by   =  1){
     Fseq <- c( 1.0,  1.1,  1.2,  1.4,  1.6,  1.8,
                2.0,  2.2,  2.5,  2.8,  3.2,  3.5,
                4.0,  4.5,  5.0,  5.6,  6.3,  7.1,
@@ -23,18 +49,29 @@ f.values <- function(from = 1.0,
               16.0, 18.0, 20.0, 22.0, 25.0, 28.0,
               32.0, 36.0, 40.0, 44.0, 50.0, 56.0,
               64.0)
-    fs.by <- Fseq[seq(1,length(Fseq), by=by)]
+    fs.by <- Fseq[seq(1, length(Fseq), by=by)]
 
     fss <- unique(sort(c(from, to, fs.by)))
 
     return(fss[fss >= from & fss <= to])
 }
 
-# Depth of Field (DOF)
-dof <- function(d,                   # [m]
-                f   = 85,            # [mm]
+
+##' Depth of Field (DOF)
+##'
+##' .. content for \details{} ..
+##' @title dof
+##' @param d focussing distance    x[m]
+##' @param f focal length of  lens [mm]
+##' @param F aperture
+##' @param COC circle of confusion [mm]
+##' @param verbose
+##' @return two-element vector with near and far focussing limits [m]
+##' @author Benno Pütz \email{puetz@@psych/mpg.de}
+dof <- function(d,
+                f   = 85,
                 F   = 1.2,
-                COC = 0.03,          # [mm]
+                COC = 0.03,
                 verbose = getOption('verbose')){
     H <- HFD(f, F, COC) * 1000       # convert to [mm]
     dmm <- d * 1000                  # convert to [mm]
@@ -54,6 +91,18 @@ dof <- function(d,                   # [m]
 
 # near/far focus
 #
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param f
+##' @param F
+##' @param min
+##' @param max
+##' @param COC
+##' @param ...
+##' @return
+##' @author Benno Pütz \email{puetz@@psych/mpg.de}
 get.nf <- function(f,
                    F,
                    min = 1,
@@ -73,6 +122,16 @@ get.nf <- function(f,
     return(nf[nf[,1]<=max, ])
 }
 
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param f
+##' @param F0
+##' @param Fn
+##' @param ...
+##' @return
+##' @author Benno Pütz \email{puetz@@psych/mpg.de}
 get.all.nf <- function(f, F0, Fn=22, ...){
     Fs <- f.values(F0, Fn, by=3)
     sq <- seq_along(Fs)
@@ -86,6 +145,16 @@ get.all.nf <- function(f, F0, Fn=22, ...){
     return(all.nf)
 }
 
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param nf
+##' @param f
+##' @param add
+##' @param ...
+##' @return
+##' @author Benno Pütz \email{puetz@@psych/mpg.de}
 plot.nf <- function(nf, f=NULL, add = FALSE, ...){
     if (!add){
         plot(c(min(nf[,1]), max(nf[,1])),
@@ -102,6 +171,17 @@ plot.nf <- function(nf, f=NULL, add = FALSE, ...){
     lines(nf[,1], nf[,3], ...)
 }
 
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param f
+##' @param F0
+##' @param Fn
+##' @param ...
+##' @param maxfr
+##' @return
+##' @author Benno Pütz \email{puetz@@psych/mpg.de}
 full.nf.plot <- function(f, F0, Fn=22, ..., maxfr=NULL){
     #Fseq <- c(1.0, 1.2, 1.4, 1.6, 1.8, 2, 2.5, 2.8, 3.5, 4, 4.5, 5, 5.6, 6.3, 7.1, 8,
     #)
@@ -137,8 +217,19 @@ full.nf.plot <- function(f, F0, Fn=22, ..., maxfr=NULL){
     return(invisible(all.nf))
 }
 
-# DOF plots
-#
+## DOF plots
+##
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param nf
+##' @param f
+##' @param add
+##' @param y.ext
+##' @param ...
+##' @return
+##' @author Benno Pütz \email{puetz@@psych/mpg.de}
 dof.plot <- function(nf, f, add = FALSE, y.ext = 1, ...){
     if (!add){
         plot(c(min(nf[,1]), max(nf[,1])),
@@ -154,7 +245,18 @@ dof.plot <- function(nf, f, add = FALSE, y.ext = 1, ...){
     lines(nf[,1], nf[,4], ...)
 }
 
-
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param f
+##' @param F0
+##' @param Fn
+##' @param maxfr
+##' @param y.ext
+##' @param ...
+##' @return
+##' @author Benno Pütz \email{puetz@@psych/mpg.de}
 full.dof.plot <- function(f, F0, Fn=22, maxfr=NULL, y.ext = 1, ...){
     #Fseq <- c(1.0, 1.2, 1.4, 1.6, 1.8, 2, 2.5, 2.8, 3.5, 4, 4.5, 5, 5.6, 6.3, 7.1, 8,
     #)
@@ -186,8 +288,18 @@ full.dof.plot <- function(f, F0, Fn=22, maxfr=NULL, y.ext = 1, ...){
 }
 
 
-# Hyperfocal distance
-#
+## Hyperfocal distance
+##
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param f
+##' @param F
+##' @param COC
+##' @param ...
+##' @return
+##' @author Benno Pütz \email{puetz@@psych/mpg.de}
 HFD <- function(f,
                 F,
                 COC = 0.03,
@@ -195,6 +307,14 @@ HFD <- function(f,
     return((f^2/(F*COC) + f)/1000)  # convert to [m] !
 }
 
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param f
+##' @param ...
+##' @return
+##' @author Benno Pütz \email{puetz@@psych/mpg.de}
 HFD.plot <- function(f,           # [mm]
                      F0  = 1,
                      COC = 0.03,  # [mm]
@@ -257,18 +377,33 @@ HFD.plot <- function(f,           # [mm]
     }
 }
 
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param ...
+##' @return
+##' @author Benno Pütz \email{puetz@@psych/mpg.de}
 Hyperfocal.plot <- function(...){
-    lenses <- cbind(f=c(400,200,100,85,70,50,24,17,8),
-                    F=c(5.6,2.8,4,1.2,2.8,1.4,2.8,4,4))
+    lenses <- cbind(f = c(400, 200,100,  85,  70,  50,  24, 17, 8),
+                    F = c(5.6, 2.8,  4, 1.2, 2.8, 1.4, 2.8,  4, 4))
     n <- nrow(lenses)
     f.lengths <- rev(lenses[,1])
     nfl <- nchar(f.lengths)
     max.nfl <- max(nfl)
-    fills <- sapply(nfl, function(i)substr('        ', 1, 2*(max.nfl - (i-1))))
+    fills <- sapply(nfl,
+                    function(i)substr('        ',
+                                      1,
+                                      2*(max.nfl - (i-1))))
     fl.str <- paste0(fills, f.lengths)
-    cols <- rainbow(n, end = 0.6)
+    cols <- rainbow(n,
+                    end = 0.6)
     for(i in 1:n){
-        HFD.plot(lenses[i,1], lenses[i,2], col = cols[i], ..., add = i>1)
+        HFD.plot(lenses[i,1],
+                 lenses[i,2],
+                 col = cols[i],
+                 ...,
+                 add = i>1)
     }
     legend('bottomleft',
            legend = fl.str,
